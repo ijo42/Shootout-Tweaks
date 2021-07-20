bb271d7e0ad478ab8ad3b5c563875e41a8f14fce
ijo42 <admin@ijo42.ru>
2021-07-20 16:24:13 +0500

    fix: fall damage must be received

--- src/main/java/ru/ijo42/shootout/damagedisplay/DamageListener.java
+++ src/main/java/ru/ijo42/shootout/damagedisplay/DamageListener.java
@@ -11,24 +11,25 @@ public class DamageListener {
 
     @SubscribeEvent
     public void onDamage(LivingDamageEvent ev){
-        if(ev.getSource().getTrueSource() instanceof EntityPlayerMP && ev.getEntity() instanceof EntityPlayerMP) {
-            final EntityPlayerMP dealer = (EntityPlayerMP) ev.getSource().getTrueSource();
+        if (ev.getEntity() instanceof EntityPlayerMP) {
             final EntityPlayerMP receiver = (EntityPlayerMP) ev.getEntity();
 
-            ShootOutNetworkWrapper.INSTANCE.sendTo(
-                    new DamageDisplayMessage(Math.min(ev.getAmount(), receiver.getHealth()),
-                            dealer.getGameProfile().hashCode()),
-                    receiver);
+            if (ev.getSource().getTrueSource() instanceof EntityPlayerMP) {
+                final EntityPlayerMP dealer = (EntityPlayerMP) ev.getSource().getTrueSource();
 
-            ShootOutNetworkWrapper.INSTANCE.sendTo(
-                    new DamageDisplayMessage(Math.min(ev.getAmount(), receiver.getHealth()), -1),
-                    dealer);
-        } else if(!(ev.getSource().getTrueSource() instanceof EntityCreature) && ev.getEntity() instanceof EntityPlayerMP){
-            final EntityPlayerMP receiver = (EntityPlayerMP) ev.getEntity();
+                ShootOutNetworkWrapper.INSTANCE.sendTo(
+                        new DamageDisplayMessage(Math.min(ev.getAmount(), receiver.getHealth()),
+                                dealer.getGameProfile().hashCode()),
+                        receiver);
 
-            ShootOutNetworkWrapper.INSTANCE.sendTo(
-                    new DamageDisplayMessage(Math.min(ev.getAmount(), receiver.getHealth()), -1),
-                    receiver);
+                ShootOutNetworkWrapper.INSTANCE.sendTo(
+                        new DamageDisplayMessage(Math.min(ev.getAmount(), receiver.getHealth()), -1),
+                        dealer);
+            } else if (!(ev.getSource().getTrueSource() instanceof EntityCreature)) {
+                ShootOutNetworkWrapper.INSTANCE.sendTo(
+                        new DamageDisplayMessage(Math.min(ev.getAmount(), receiver.getHealth()), receiver.getGameProfile().hashCode()),
+                        receiver);
+            }
         }
     }
 }
